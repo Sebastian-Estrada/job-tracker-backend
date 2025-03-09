7f3932d7ccbe0d7e245e5008c778c241f160445d
Sebastian Estrada <sestrada@danalyticspro.co>
2025-03-09 11:15:03 -0400

    Fix import of Company on command

--- apps/applications/management/commands/data_loading.py
+++ apps/applications/management/commands/data_loading.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from applications.models import Company
+from apps.applications.models import Company
 from faker import Faker
 
 class Command(BaseCommand):
