600fabd0d044eba99cf8ad4eb9b4057b000aa3ba
Esther Barnor <barnor.esther@gmail.com>
2025-04-01 15:31:18 -0400

    adding dashboard

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 import environ
 
+env = environ.Env()
+environ.Env.read_env()
+
 from pathlib import Path
 from decouple import config
 
